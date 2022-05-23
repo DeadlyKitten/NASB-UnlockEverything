bef66b29c6a3bda71bbc7b0c726224cad3d7e58f
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-05-23 16:22:56 -0700

    Add project files.

---
+++ UnlockEverything/ReflectionUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace UnlockEverything
+{
+    public static class ReflectionUtil
+    {
+        private const BindingFlags _allBindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+
+
+        //Invokes a (static?) private method with name "methodName" and params "methodParams", returns an object of the specified type
+        public static T InvokeMethod<T>(this object obj, string methodName, params object[] methodParams) => (T)InvokeMethod(obj, methodName, methodParams);
+
+        //Invokes a (static?) private method with name "methodName" and params "methodParams"
+        public static object InvokeMethod(this object obj, string methodName, params object[] methodParams)
+        {
+            MethodInfo method = obj.GetType().GetMethod(methodName, _allBindingFlags);
+            if (method == null)
+                throw new InvalidOperationException($"{methodName} is not a member of {obj.GetType().Name}");
+            return method.Invoke(obj, methodParams);
+        }
+    }
+}
---
+++ UnlockEverything/UnlockEverythingPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BepInEx;
+using Nick;
+using System.Collections;
+using UnityEngine;
+using System.Linq;
+using BepInEx.Logging;
+
+namespace UnlockEverything
+{
+    [BepInPlugin("com.steven.nasb.unlockeverything", "Unlock Everything", "1.0.0")]
+    public class UnlockEverythingPlugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            StartCoroutine(TriggerUnlockEverything());
+        }
+
+        IEnumerator TriggerUnlockEverything()
+        {
+            UnlockCheats uc = null;
+
+            yield return new WaitUntil(() => uc = Resources.FindObjectsOfTypeAll<UnlockCheats>().FirstOrDefault());
+
+            uc.InvokeMethod("UnlockEverything");
+
+            Logger.Log(LogLevel.Info, "Unlocked everything!");
+        }
+    }
+}
